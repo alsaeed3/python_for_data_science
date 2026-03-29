d7323f35c2dbe4fba3701ac6ba8dbd8a36c10933
alsaeed3 <ali.eissa.570@gmail.com>
2026-03-29 14:07:58 +0400

    Add Hello.py with data structure manipulations and print statements

---
+++ ex00/Hello.py
@@ -0,0 +1,22 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+# List manipulation
+ft_list[1] = "World!"
+
+# Tuple manipulation
+ft_tuple = ("Hello", "United Arab Emirates!")
+
+# Set manipulation
+ft_set.remove("tutu!")
+ft_set.add("Abu Dhabi!")
+
+# Dictionary manipulation
+ft_dict["Hello"] = "42AbuDhabi!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
